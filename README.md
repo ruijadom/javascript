ddcd4ad13c614ed657bcc9edc9f37cc844fe8011
Rui Domingues <ruijadomingues@gmail.com>
2019-10-10 10:03:59 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,11 @@
+let certsToAdd = ["Algorithms and Data Structures", "Front end Libraries"];
+
+let certificates = [
+  "Responsive Web Design",
+  ...certsToAdd,
+  "Data Vizualization",
+  "Data Mining",
+  "Cyber Security"
+];
+
+console.log(certificates);
